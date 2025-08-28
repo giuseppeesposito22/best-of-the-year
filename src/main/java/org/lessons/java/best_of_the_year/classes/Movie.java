1a38882091a6a3a2c970c372484b2f2d0912f182
giuseppeesposito22 <gius.espo22@gmail.com>
2025-08-28 12:56:03 +0200

    create Movie and Song classes

---
+++ src/main/java/org/lessons/java/best_of_the_year/classes/Movie.java
@@ -0,0 +1,48 @@
+package org.lessons.java.best_of_the_year.classes;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable{
+    
+    private int id;
+    private String title;
+
+
+    public Movie(){};
+
+
+    public Movie(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    
+
+
+    
+
+
+
+
+}
---
+++ src/main/java/org/lessons/java/best_of_the_year/classes/Song.java
@@ -0,0 +1,43 @@
+package org.lessons.java.best_of_the_year.classes;
+
+import java.io.Serializable;
+
+public class Song implements Serializable{
+
+    private int id;
+    private String title;
+
+
+
+    public Song(){};
+
+
+    public Song(int id, String title){
+        this.id = id;
+        this.title = title;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    
+
+
+}
