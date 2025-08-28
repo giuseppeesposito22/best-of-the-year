b206dfe258e2b2b981f5c4debcb4ae7f89a9d776
giuseppeesposito22 <gius.espo22@gmail.com>
2025-08-28 13:46:56 +0200

    add step 2

--- src/main/java/org/lessons/java/best_of_the_year/controllers/HomeController.java
+++ src/main/java/org/lessons/java/best_of_the_year/controllers/HomeController.java
@@ -1,5 +1,9 @@
 package org.lessons.java.best_of_the_year.controllers;
 
+import java.util.ArrayList;
+
+import org.lessons.java.best_of_the_year.classes.Movie;
+import org.lessons.java.best_of_the_year.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +17,66 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("name", "Giuseppe Esposito" );
         return "home";
+    }
+
+
+    private ArrayList<Movie> getBestMovies(){
+        
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        movies.add(new Movie(1, "Interstellar"));
+        movies.add(new Movie(2, "Inception"));
+        movies.add(new Movie(3, "Karate Kid"));
+        movies.add(new Movie(4, "The Matrix"));
+
+        return movies;
+    }
+
+
+    private ArrayList<Song> getBestSongs(){
+        ArrayList<Song> songs = new ArrayList<>();
+        songs.add(new Song(1, "Bohemian Rhapsody"));
+        songs.add(new Song(2, "Imagine"));
+        songs.add(new Song(3, "Stairway to Heaven"));
+        songs.add(new Song(4, "In the End"));
+
+        return songs;
+
+    }
+
+    @GetMapping("/movies")
+    public String movies(Model model){
+
+        StringBuilder movieTitles = new StringBuilder();
+       for (Movie movie : getBestMovies()) {
+        if(movieTitles.length() > 0){
+            movieTitles.append(", ");
+
+        }
+        movieTitles.append(movie.getTitle());
+       }
+
+       model.addAttribute("movieTitles", movieTitles.toString());
+
+
+    return "movies";
+    }
+
+    @GetMapping("/songs")
+    public String songs(Model model){
+
+        StringBuilder songTitles = new StringBuilder();
+
+        for (Song song : getBestSongs()) {
+
+            if(songTitles.length() > 0){
+                songTitles.append(", ");
+            }
+            songTitles.append(song.getTitle());
+        }
+
+        model.addAttribute("songTitles", songTitles.toString());
+
+    return "songs";
     }
 }
